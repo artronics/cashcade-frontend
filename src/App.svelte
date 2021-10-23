2232b58b5677e37c55da00c80191b968ec56b3d5
Jalal <artronics@Jalals-MacBook-Pro.local>
2021-10-23 18:42:16 +0100

    Initial Commit

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+  import Nav from "./components/Nav.svelte"
+</script>
+
+<main>
+  <Nav/>
+
+</main>
+
+<style global lang="postcss">
+    @tailwind base;
+    @tailwind components;
+    @tailwind utilities;
+</style>
---
+++ src/components/Nav.svelte
@@ -0,0 +1,9 @@
+<script>
+ let count = 0;
+  const foo = () => count++
+  $: console.log("kir" + count)
+
+</script>
+<nav on:click={foo}>
+  kir {count}
+</nav>
